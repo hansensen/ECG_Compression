70b9ba536be6c132d2434e8dbc6c0dea858cec6a
Han Jie <hanjie1030@gmail.com>
2016-10-08 17:50:46 +0800

    Initial version of complete compression program
    Oct 8 2016 at Biolab.

--- compression.m
+++ compression.m
@@ -2,15 +2,15 @@
 %          1. Linear Predictive Coding
 %          2. Coding-packing Scheme
 %          ***********************************************************
-%          * A * 1 *   5/4 bits     *   5/4 bits    *    5/4 bits    *
+%        3 * A * 1 *   5/4 bits     *   5/4 bits    *    5/4 bits    *
 %          ***********************************************************
-%          * B * 0 1 *      7/6 bits         *      7/6 bits         *
+%        4 * B * 0 1 *      7/6 bits         *      7/6 bits         *
 %          ***********************************************************
-%          * C * 0 0 0 1 *  3 bits  *  3 bits  *  3 bits  *  3 bits  *
+%        2 * C * 0 0 0 1 *  3 bits  *  3 bits  *  3 bits  *  3 bits  *
 %          ***********************************************************
-%          * D * 0 0 0 0 *2 bits *2 bits *2 bits*2 bits*2 bits*2 bits*
+%        1 * D * 0 0 0 0 *2 bits *2 bits *2 bits*2 bits*2 bits*2 bits*
 %          ***********************************************************
-%          * E * 0 0 1 1 *              12 bits                      *
+%        5 * E * 0 0 1 1 *              12 bits                      *
 %          ***********************************************************
 %
 %   Clear our variables
@@ -19,8 +19,8 @@ clear
 plotname = 'Lead 1';
 load ecg.mat;
 lead1 = ecgdata(:,2);
-%size = length(lead1);
-size = 1000;
+size = length(lead1);
+%size = 24;
 
 lead11(1) = lead1(1);
 lead11(2) = lead1(2);
@@ -33,38 +33,66 @@ title('\bf1. Original ECG'); ylim([-300 300]);
 subplot(2, 1, 2); plot(lead11);
 title('\bf2. Linear Predictive ECG'); ylim([-50 50]);
 
-for i = 1:6:size
-    if (size < i+6)
-        endpoint = size;
-    else
-        endpoint = i + 6;
-    end
-    %max = 0;
+k = 0;
+m = 0;
+i = 1;
+ecgout(:) = 0;
 
-    %for j = i:1:endpoint
-    %    if (abs(lead11(j))>max)
-    %        max = abs(lead11);
-    %    end
-    %end
-    maxi = max(lead11(i:endpoint));
-
-    
-    frameFormat = 5;
-    if ((maxi>=-2) & (maxi<=1))
-        frameFormat = 1;
-    else if ((maxi>=-4) & (maxi<=3))
-        frameFormat = 2;
-    else if ((maxi>=-8) & (maxi<=7)) 
-        frameFormat = 3;
-    else if ((maxi>=-16) & (maxi<=15))
-        frameFormat = 4;
-    else if ((maxi>=-64) & (maxi<=63)) 
-        frameFormat = 5;
+while (i<=(size-5))
+    ecgo = 0;
+    %Frame D
+    if ((max(lead11(i:(i+5)))>=-2) && (max(lead11(i:(i+5)))<=1))
+        for j=i:1:(i+5)
+            ecgo = bitset(ecgo,(j-i)*2+1,bitget(bin2dec(dec2twos(lead11(j))),1));
+            ecgo = bitset(ecgo,(j-i)*2+2,bitget(bin2dec(dec2twos(lead11(j))),2));
         end
+        ecgo = bitand(ecgo,4095);
+        i = i + 6;
+        
+    %Frame C
+    else if ((max(lead11(i:(i+3)))>=-4) && (max(lead11(i:(i+3)))<=3))
+        for j=i:1:(i+3)
+            ecgo = bitset(ecgo,(j-i)*3+1,bitget(bin2dec(dec2twos(lead11(j))),1));
+            ecgo = bitset(ecgo,(j-i)*3+2,bitget(bin2dec(dec2twos(lead11(j))),2));
+            ecgo = bitset(ecgo,(j-i)*3+3,bitget(bin2dec(dec2twos(lead11(j))),3));
+        end
+        ecgo = bitand(ecgo,4095);
+        ecgo = bitset(ecgo,13,1);
+        i = i + 4;
+
+    %Frame A
+    else if ((max(lead11(i:(i+2)))>=-16) && (max(lead11(i:(i+2)))<=15))
+        %do something
+        for j=i:1:(i+2)
+            ecgo = bitxor(ecgo,bitshift(bin2dec(dec2twos(lead11(j))),(j-i)*5));
+        end
+        ecgo = bitset(ecgo,16,1);
+        i = i + 3;
+        
+    %Frame B
+    else if ((max(lead11(i:(i+1)))>=-64) && (max(lead11(i:(i+1)))<=63)) 
+        %do something
+        for j=i:1:(i+1)
+            ecgo = bitxor(ecgo,bitshift(bin2dec(dec2twos(lead11(j))),(j-i)*7));
+        end
+        ecgo = bitset(ecgo,16,0);
+        ecgo = bitset(ecgo,15,1);
+        i = i + 2;
+        
+    %Frame E
+        else
+        ecgo = bin2dec(dec2twos(lead11(j)));
+        ecgo = bitset(ecgo,16,0);
+        ecgo = bitset(ecgo,15,0);
+        ecgo = bitset(ecgo,14,1);
+        ecgo = bitset(ecgo,13,1);     
+        i = i + 1;
         end
         end
         end
     end
-    %Data Packing
-    disp(frameFormat);
+    m = m+1;
+    ecgout(m,1) = ecgo;
 end
+
+csvwrite('Compressed_ECG.csv',ecgout);
